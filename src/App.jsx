7ef89c308fa3704f305b25cc277bf64267a69b91
ikmalif29 <ikmalfauzaeni@gmail.com>
2024-09-12 13:53:21 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Profile from "./Components/Profile"
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h2>My Profile</h2>
+      </header>
+      <Profile />
+    </>
+  )
+}
+
+export default App
